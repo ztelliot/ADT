b2db577b7b8252769ef9d6e1c4723c2d45e60af7
ztelliot <1141480995@qq.com>
2020-02-11 13:40:54 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from wsgiref.simple_server import make_server
 from flaskapp import app as application
+import subprocess
 if __name__ == "__main__":
+    sta = subprocess.getstatus('env >> /etc/default/locale && /etc/init.d/cron start && nginx start')
+    if sta == 0:
+        print('Env Started!')
     with open('config.json', 'r') as rf:
         config = rf.read()
     rf.close()
